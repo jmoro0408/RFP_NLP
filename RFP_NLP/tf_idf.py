585ad81afe50aebc29db9c8ef7a3d31a60ff8426
jmoro0408 <james.moro.11@gmail.com>
2022-01-01 12:30:56 +0000

    started tf_idf processing

---
+++ RFP_NLP/tf_idf.py
@@ -0,0 +1,6 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from pathlib import Path
+from text_extract import TEXT_DIR
+
+print(TEXT_DIR)
