8b7521003a81e7bea77eac1893d0e1393ce4c161
jmoro0408 <james.moro.11@gmail.com>
2022-01-01 16:37:35 +0000

    addec cosine similarity function

--- RFP_NLP/tf_idf.py
+++ RFP_NLP/tf_idf.py
@@ -1,5 +1,4 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import linear_kernel
 from pathlib import Path
 from text_extract import TEXT_DIR
@@ -7,6 +6,7 @@ from typing import Union
 import pandas as pd
 
 file_token_dict = {}
+
 
 def get_txts(path: Union[Path, str]) -> list:
     txt_list = [p for p in path.rglob("*") if p.is_file() and p.match("*.txt")]
@@ -17,6 +17,8 @@ def get_txts(path: Union[Path, str]) -> list:
 
 def tokenize(txt_file):
     tfidf_vectorizer = TfidfVectorizer(input="filename", stop_words="english")
+    if not isinstance(txt_file, list):
+        txt_file = [txt_file]
     tfidf_vector = tfidf_vectorizer.fit_transform(txt_file)
     df_tfidf_sklearn = pd.DataFrame(
         tfidf_vector.toarray(), columns=tfidf_vectorizer.get_feature_names_out()
@@ -24,14 +26,20 @@ def tokenize(txt_file):
     df_tfidf_sklearn = df_tfidf_sklearn.T
     return tfidf_vector, df_tfidf_sklearn
 
-def add_to_tokens_dict():
-    
+
+def add_to_tokens_dict(title, tfidf_vector):
+    file_token_dict[title] = tfidf_vector
+    return file_token_dict
 
 
-
+def calculate_cosine_sim(matrix1, matrix2):
+    cosine_sim = linear_kernel(matrix1, matrix2)
+    return cosine_sim
 
 
 if __name__ == "__main__":
     txts, titles = get_txts(TEXT_DIR)
-    tfidf, tfidf_df = tokenize(txts)
-    print(tfidf_df.sort_values(by=0, ascending=False, axis=0))
+    for txt, title in zip(txts, titles):
+        tfidf, tfidf_df = tokenize(txt)
+        add_to_tokens_dict(txt, tfidf)
+    print(file_token_dict)
