5bc4943ae198473b6196895ba7757aadc9019327
jmoro0408 <james.moro.11@gmail.com>
2022-01-02 18:28:14 +0000

    Implement tfidf
    First draft implementation of the tf idf algorithm.
    This seems to work for document similarity, although requires some
    tidying up.

--- RFP_NLP/tf_idf.py
+++ RFP_NLP/tf_idf.py
@@ -4,8 +4,8 @@ from pathlib import Path
 from text_extract import TEXT_DIR
 from typing import Union
 import pandas as pd
-from scipy.sparse import hstack, vstack, csr_matrix
-from tabulate import tabulate
+from prepare_base_doc import BASE_DOC_DIR
+
 
 def get_txts(path: Union[Path, str]) -> list:
     file_token_dict = {}
@@ -22,57 +22,49 @@ def get_txts(path: Union[Path, str]) -> list:
     return txt_df
 
 
-# def tokenize(text_df):
-#     txt_content = text_df["content"]
-#     # Create TfidfVectorizer object
-#     vectorizer = TfidfVectorizer(stop_words="english")
-#     # Generate matrix of word vectors
-#     tfidf_matrix = vectorizer.fit_transform(txt_content)
-#     # Print the shape of tfidf_matrix
-#     print(f"tfidf matrix has a shape of {tfidf_matrix.shape}")
-#     return tfidf_matrix
+def get_base_document_content(TEXT_DIR):
+    TEXT_DIR = Path(TEXT_DIR.parent)
+    txt_list = [p for p in TEXT_DIR.rglob("*") if p.is_file() and p.match("*.txt")]
+    assert (
+        len(txt_list) == 1
+    ), "Please ensure only one file is being provided for comparison"
+    txt_file = txt_list[0]
+    with open(txt_file) as f:
+        data = f.read()
+    return data
 
-
-# def calculate_cosine_sim(matrix1, matrix2):
-#     return linear_kernel(matrix1, matrix2)
-
-
-# def compare_cosine(matrix1, matrix2):
-#     mising_length = abs(matrix1.shape[1] - matrix2.shape[1])
-#     if matrix1.shape[1] < matrix2.shape[1]:
-#         matrix1 = vstack((matrix2, csr_matrix((mising_length, matrix2.shape[1]))))
-#     elif matrix1.shape[1] < matrix2.shape[1]:
-#         matrix2 = vstack((matrix1, csr_matrix((mising_length, matrix1.shape[1]))))
-#     cosine_sim = calculate_cosine_sim(matrix1, matrix2)
-#     print(type(matrix1))
-#     return cosine_sim
 
 def process_tfidf_similarity(input_text_df, base_document):
-    corpus = list(input_text_df['content'])
-	vectorizer = TfidfVectorizer()
+    corpus = input_text_df["content"].tolist()
+    vectorizer = TfidfVectorizer(stop_words="english")
 
-	# To make uniformed vectors, both documents need to be combined first.
-	corpus.insert(0, base_document)
-	embeddings = vectorizer.fit_transform(corpus)
+    # To make uniformed vectors, both documents need to be combined first.
+    corpus.insert(0, base_document)
+    embeddings = vectorizer.fit_transform(corpus)
 
-	cosine_similarities = cosine_similarity(embeddings[0:1], embeddings[1:]).flatten()
+    cosine_similarities = cosine_similarity(embeddings[0:1], embeddings[1:]).flatten()
 
-	highest_score = 0
-	highest_score_index = 0
-	for i, score in enumerate(cosine_similarities):
-		if highest_score < score:
-			highest_score = score
-			highest_score_index = i
+    highest_score = 0
+    highest_score_index = 0
+    for i, score in enumerate(cosine_similarities):
+        if highest_score < score:
+            highest_score = score
+            highest_score_index = i
 
-
-	most_similar_document = corpus[highest_score_index]
-
-	print("Most similar document by TF-IDF with the score:", most_similar_document, highest_score)
-
+    most_similar_document_content = corpus[highest_score_index]
+    most_similar_document_title = str(
+        input_text_df[
+            input_text_df["content"].str.contains(
+                most_similar_document_content, case=False, regex=False
+            )
+        ]["title"]
+    )
+    print(
+        f"Most similar document by TF-IDF:{most_similar_document_title}, with the score:{round(highest_score,3)}"
+    )
 
 
 if __name__ == "__main__":
     txt_df = get_txts(TEXT_DIR)
-    document_to_compare = read_base_document
-    process_tfidf_similarity()
-
+    document_to_compare = get_base_document_content(BASE_DOC_DIR)
+    process_tfidf_similarity(input_text_df=txt_df, base_document=document_to_compare)
