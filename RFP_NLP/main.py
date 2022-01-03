448a6050b740627dc9bc1392c2341ca7209f6926
jmoro0408 <james.moro.11@gmail.com>
2022-01-03 09:54:26 +0000

    Initial commit of combined main file
    This file will be the main entry point for the program and will import
    utils functions from other helper files.

---
+++ RFP_NLP/main.py
@@ -0,0 +1,16 @@
+from tf_idf import get_txts, get_base_document_content, process_tfidf_similarity
+from prepare_base_doc import 
+
+
+
+## Check analysed PDFs for any missing
+#   -> Analyse any missing
+
+## Import document to be checked
+#   -> Import PDF and output .txt
+
+## Import new .txt file
+
+## Concatenate document to be checked with corpus of documents
+
+## Process similarity with existing corpus
