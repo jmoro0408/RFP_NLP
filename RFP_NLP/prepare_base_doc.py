613153348ff0c0cd5b1f8442d2a229f732f19693
jmoro0408 <james.moro.11@gmail.com>
2022-01-09 14:38:43 -0800

    Formatting changes to adhere to pylint

--- RFP_NLP/prepare_base_doc.py
+++ RFP_NLP/prepare_base_doc.py
@@ -1,9 +1,17 @@
+"""Module to prepare the base document for comparison.
+In this context the base document is the documne that is being used for comparison.
+This file identifies the pdf to be compared (from the directory specified in doc_locations.py) and
+extracts the text, cleans the text, and saves it in the same directory.
+"""
+
+
 from pathlib import Path
 from text_extract import extract_text_from_pdf, save_txt_file, remove_breaks_and_dedent
 from doc_locations import DOC_TO_COMPARE_DIR
 
 
 def prepare_base_doc():
+    """See module docstring."""
     pdf_list = [
         p for p in DOC_TO_COMPARE_DIR.rglob("*") if p.is_file() and p.match("*.pdf")
     ]
