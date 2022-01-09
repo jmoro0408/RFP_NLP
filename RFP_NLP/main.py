9303894d9a0a316bd4b857e1e6023091f762fbc5
jmoro0408 <james.moro.11@gmail.com>
2022-01-09 06:43:52 -0800

    Compare previously extracted txt files with PDFs
    Added a function that compared the existing extracted text files with
    the PDF documents provided to be compared against.
    If a new PDF has been added since last run, and has not previously had
    it's text extracted it is saved as a new .txt file.

--- RFP_NLP/main.py
+++ RFP_NLP/main.py
@@ -1,10 +1,31 @@
 from tf_idf import get_txts, get_base_document_content, process_tfidf_similarity
-from prepare_base_doc import 
-
+from prepare_base_doc import *
+from pathlib import Path
+from text_extract import get_pdfs, PDF_DIR, TEXT_DIR, extract_text, save_txt_file
 
 
 ## Check analysed PDFs for any missing
 #   -> Analyse any missing
+
+
+def check_missing_txts():
+    """Compares the PDF files current analysed against any new files that may have been added since last run.
+    If a new PDF has been added, this will extract the text and save a new .txt file.
+    """
+    list_of_pdfs = get_pdfs(PDF_DIR)  # Get a list of all PDFS to be compared against
+    list_of_txts = get_txts(TEXT_DIR)[
+        "title"
+    ].to_list()  # Get list of PDFs that have already been analysed
+
+    for pdf in list_of_pdfs:
+        pdf = Path(pdf).stem
+        new_txts_analysed = 0
+        if pdf not in list_of_txts:
+            new_txt_file = extract_text(pdf)
+            save_txt_file(new_txt_file, Path(pdf).stem, TEXT_DIR)
+            new_txts_analysed += 1
+    print(f"{new_txts_analysed} new text file(s) added")
+
 
 ## Import document to be checked
 #   -> Import PDF and output .txt
@@ -14,3 +35,7 @@ from prepare_base_doc import
 ## Concatenate document to be checked with corpus of documents
 
 ## Process similarity with existing corpus
+
+
+if __name__ == "__main__":
+    check_missing_txts()
