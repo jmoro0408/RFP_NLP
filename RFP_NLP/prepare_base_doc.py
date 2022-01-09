d8be0b7ea68566455678a4fbefd085e886be6be1
jmoro0408 <james.moro.11@gmail.com>
2022-01-09 07:59:34 -0800

    Move main into function and add print statement

--- RFP_NLP/prepare_base_doc.py
+++ RFP_NLP/prepare_base_doc.py
@@ -6,7 +6,9 @@ BASE_DOC_DIR = Path(
     r"RFP_NLP/data/proposals/doc_to_compare/20_Jacobs Bissell Point Fine Screen Project 12548.pdf"
 )
 
-if __name__ == "__main__":
+
+def prepare_base_doc():
+    print(f"File to be compared: {Path(BASE_DOC_DIR).stem}.")
     extracted_text = extract_text(BASE_DOC_DIR)
     extracted_text = remove_breaks_and_dedent(extracted_text)
     save_txt_file(extracted_text, Path(BASE_DOC_DIR).stem, Path(BASE_DOC_DIR.parent))
