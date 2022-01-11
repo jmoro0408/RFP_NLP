933583da2fa2e3ef8363c681df19b009093799c3
jmoro0408 <james.moro.11@gmail.com>
2022-01-10 17:30:53 -0800

    Added from local file
    Added in the files that I previously created on my local system required
    for the main function to work.

--- Azure/RFP_NLP_trigger/__init__.py
+++ Azure/RFP_NLP_trigger/__init__.py
@@ -1,24 +1,87 @@
-import logging
+"""Main file to undertake term frequency - inverse document frequency on
+a number of PDFs and a base document to compare.
+"""
 
+from pathlib import Path
+from tf_idf import get_txts, get_base_document_content, process_tfidf_similarity
+from prepare_base_doc import prepare_base_doc
+from doc_locations import DOC_TO_COMPARE_DIR, PDF_DIR, TEXT_DIR
+from text_extract import (
+    get_pdfs,
+    extract_text_from_pdf,
+    save_txt_file,
+    remove_breaks_and_dedent,
+)
+#Azure function libraries
+import logging
 import azure.functions as func
+
+## Check analysed PDFs for any missing
+#   -> Analyse any missing
+
+
+def check_missing_txts():
+    """Compares the PDF files current analysed against any new files
+    that may have been added since last run.
+    If a new PDF has been added, this will extract the text and save a new .txt file.
+    """
+    list_of_pdfs = get_pdfs(PDF_DIR)  # Get a list of all PDFS to be compared against
+    list_of_txts = list(
+        get_txts(TEXT_DIR).keys()
+    )  # Get list of PDFs that have already been analysed
+    new_txts_analysed = 0
+    for pdf in list_of_pdfs:
+        pdf_no_suffix = Path(pdf).stem
+        if pdf_no_suffix not in list_of_txts:
+            new_txt_file = extract_text_from_pdf(pdf)
+            new_txt_file = remove_breaks_and_dedent(new_txt_file)
+            save_txt_file(new_txt_file, Path(pdf).stem, TEXT_DIR)
+            new_txts_analysed += 1
+    if new_txts_analysed == 0:
+        print("No new PDFs found.")
+    else:
+        print(f"{new_txts_analysed} new text files added.")
+
+
+## Import document to be checked
+# prepare_base_doc()
+
+
+def tf_idf():
+    """Creates document simililarity between base doc (to be compared)
+    and corpus of existing texts.
+    1. Import base doc txt file
+    2. Concatenate with existing corpus of documents
+    3. Output highest scoring documents through tf-idf algorith.
+    """
+    base_doc_txt = get_base_document_content(DOC_TO_COMPARE_DIR)
+    comparison_txt_dict = get_txts(TEXT_DIR)
+    document_similarity = process_tfidf_similarity(
+        input_text_dict=comparison_txt_dict, base_document=base_doc_txt
+    )
+    return document_similarity
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    # logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # name = req.params.get('name')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # if name:
+    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
+
+    check_missing_txts()
+    prepare_base_doc()
+    tf_idf()
---
+++ Azure/RFP_NLP_trigger/tf_idf.py
@@ -0,0 +1,122 @@
+"""Module to house main tf-idf (term frequency inverse document frequency) algorithm
+"""
+
+from pathlib import Path
+from typing import Union
+import itertools
+from pprint import pprint
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+from nltk.corpus import stopwords
+
+
+def get_txts(path: Union[Path, str]) -> dict:
+    """Function to read in all text files in a given directory and return a dict of their
+    title and content, stripped of newlines.
+
+    Args:
+        path (Union[Path, str]): Directory of txt files.
+
+    Returns:
+        dict: Dictionary housing the txt file title and content as key-value pairs.
+    """
+    path = Path(path)
+    file_token_dict = {}
+    txt_list = [
+        p for p in path.rglob("*") if p.is_file() and p.match("*.txt")
+    ]  # list of all txt documents in dir, in PosixPath format.
+    txt_titles = [
+        Path(text).stem for text in txt_list
+    ]  # grab the titles, with no parent path or file extension
+    # iterate through list of txts, read the file and grab the content,
+    # and attach the conen twith the title to a dict
+    for txt_file, title in zip(txt_list, txt_titles):
+        if title not in file_token_dict:  # only reads items that haven't been added
+            with open(txt_file, "r", encoding="utf-8") as file:
+                content = file.read().replace("\n", "")
+                file_token_dict[title] = content
+    return file_token_dict
+
+
+def get_base_document_content(base_doc_dir: Union[Path, str]) -> str:
+    """Retrives the text content of the base document to be compared.
+
+    Args:
+        base_doc_dir (Union[Path, str]): Directory containing the base document text file.
+
+    Returns:
+        str: txt content of the base document.
+    """
+    base_doc_dir = Path(base_doc_dir)
+    txt_list = [p for p in base_doc_dir.rglob("*") if p.is_file() and p.match("*.txt")]
+    assert (
+        len(txt_list) == 1
+    ), "Please ensure only one file is being provided for comparison"
+    txt_file = txt_list[0]
+    with open(txt_file, encoding="utf-8") as file:
+        data = file.read().replace("\n", "")
+    return data
+
+
+def take(num, iterable):
+    "Return first n items of the iterable as a list"
+    return list(itertools.islice(iterable, num))
+
+
+def process_tfidf_similarity(
+    input_text_dict: dict, base_document: str, top_n_docs: int = 10
+) -> dict:
+    """Function to undertake the tf-idf similirity processing.
+    This function takes in a dictionary of documents to be compared against, with the format:
+    {doc_title : doc_content}. It compares this to the base document content to be compared.
+    The function uses the sci-kit learn implementation of tf-idf:
+    (https://bit.ly/3nchURz)
+
+    The function return a dictionary of the most similar documents, complete with their scores.
+    The top N (currently set at 10) most similar documents are then printed, however the entire
+    ditionary with all documents and scores are returned.
+
+    Args:
+        input_text_dict (dict): Dict with titles and content of documents to be compared against
+        base_document str: text content of base document to be compared
+
+    Returns:
+        dict: dict of most sorted document similarity with format title : score
+    """
+    # Using the nltk stopwords corpus instead of the sklearn built in stopwords,
+    # see https://bit.ly/3tdqsLQ for reasoning behind this
+    nltk.download("stopwords")
+    nltk_stop_words = stopwords.words("english")
+    # nltk_stop_words = [word.decode("utf-8") for word in stopwords.words("english")]
+
+    vectorizer = TfidfVectorizer(stop_words=nltk_stop_words)
+
+    # input_txt_dict should be a dictionary containing documents to be compared against.
+    # keys: inidividual document titles, values: individual document content
+    doc_corpus = list(
+        input_text_dict.values()
+    )  # list of input_text_dict_values. Same length as input dict.
+    # To make uniformed vectors, both documents need to be combined first.
+    doc_corpus.insert(0, base_document)
+    doc_corpus_title_slices = list(
+        input_text_dict.keys()
+    )  # Have to be able to slice in order to grab the title with the highest scoring index
+    embeddings = vectorizer.fit_transform(doc_corpus)
+    cosine_similarities = cosine_similarity(embeddings[0:1], embeddings[1:]).flatten()
+    score_df_dict = {}
+    for i, score in enumerate(cosine_similarities):
+        score_title = doc_corpus_title_slices[i]
+        score_df_dict[score_title] = round(score, 5)
+
+    sorted_score_dict = dict(
+        sorted(score_df_dict.items(), key=lambda item: item[1], reverse=True)
+    )
+    top_n_scores = {
+        k: sorted_score_dict[k] for k in list(sorted_score_dict)[:top_n_docs]
+    }
+
+    pprint(
+        f"Highest scoring documents are score are: {top_n_scores}"
+    )
+    return sorted_score_dict
