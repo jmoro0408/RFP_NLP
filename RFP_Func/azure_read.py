60f3f35b6f26761b9f4798fa94dca54c90355488
jmoro0408 <james.moro.11@gmail.com>
2022-01-17 14:52:40 -0800

    Added azure blob upload
    added capability to upload file to azure container

--- RFP_Func/azure_read.py
+++ RFP_Func/azure_read.py
@@ -47,7 +47,7 @@ def start_computervision_client(computer_vision_key, computer_vision_endpoint):
     return ComputerVisionClient(computer_vision_endpoint, CognitiveServicesCredentials(computer_vision_key))
 
 def call_read_api(blob_url, computervision_client):
-    print("======= Text Extraction =======")
+    print("======= Starting Text Extraction =======")
     # Call API with URL and raw response (allows you to get the operation location)
     read_response = computervision_client.read(blob_url,  raw=True)
     # Get the operation location (URL with an ID at the end) from the response
@@ -66,11 +66,16 @@ def save_read_result(read_result, save_filename):
     # save the detected text, line by line
     if read_result.status == OperationStatusCodes.succeeded:
         for text_result in read_result.analyze_result.read_results:
-            with open(f'{save_filename}.txt', 'w') as f:
+            with open(save_filename, 'w') as f:
                 for line in text_result.lines:
                     f.write(f'{line.text}\n')
         print(f'Extracted text saved as {save_filename}.txt')
 
+def upload_to_container(container_client, filename):
+    print(f'======= Uploading to {container_client.container_name} container =======')
+    with open(filename, "rb") as upload_data:
+        container_client.upload_blob(name = filename, data = upload_data)
+    print(f'File {filename} successfully saved to {container_client.container_name}')
 
 def main():
     load_dotenv()
@@ -87,15 +92,20 @@ def main():
     computervision_client = start_computervision_client(vision_key, vision_endpoint)
 
     # get raw base doc (rfp) input
-    raw_rf_container_client = start_blob_container_client('raw-rfp', blob_service_client)
-    raw_rfp_url = list(get_blob_url(raw_rf_container_client, blob_sas_token).values())[0]
-    raw_rfp_filename = list(get_blob_url(raw_rf_container_client, blob_sas_token).keys())[0]
-    raw_rfp_filename = os.path.splitext(raw_rfp_filename)[0]
+    raw_rfp_container_client = start_blob_container_client('raw-rfp', blob_service_client)
+    raw_rfp_url = list(get_blob_url(raw_rfp_container_client, blob_sas_token).values())[0]
+    raw_rfp_filename = list(get_blob_url(raw_rfp_container_client, blob_sas_token).keys())[0]
+    raw_rfp_filename = os.path.splitext(raw_rfp_filename)[0] + '.txt'
 
     #get raw rfp text
-    rfp_read_result = call_read_api(raw_rfp_url, computervision_client)
+    #rfp_read_result = call_read_api(raw_rfp_url, computervision_client)
 
     #save extracted text
-    save_read_result(rfp_read_result, raw_rfp_filename)
+    #save_read_result(rfp_read_result, raw_rfp_filename)
+
+    #upload to process rfp container
+    processed_rfp_container_client = start_blob_container_client('processed-rfp', blob_service_client)
+    filepath = r'/Users/jamesmoro/Documents/Python/RFP_NLP/RFP_Func/sample.txt'
+    upload_to_container(processed_rfp_container_client, raw_rfp_filename)
 
 main()
