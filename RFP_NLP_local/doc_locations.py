8222151e392a4b306a3f266c9c520170ce45977c
jmoro0408 <james.moro.11@gmail.com>
2022-01-22 15:31:46 -0800

    Moved to local only
    Created a folder on my desktop to hold local stuff. Previous versions of
    the algorithm etc

--- RFP_NLP_local/doc_locations.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-DOC_TO_COMPARE_DIR = Path(r"RFP_NLP/data/proposals/doc_to_compare/")
-PDF_DIR = Path(r"/Users/jamesmoro/Documents/Python/RFP_NLP/RFP_NLP/data/proposals/raw")
-TEXT_DIR = Path(r"RFP_NLP/data/proposals/text_only")
